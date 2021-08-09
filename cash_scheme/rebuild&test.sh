1b1a6066ba0105a48a5f4ce31d6b55a2e32f101d
collision <collisionl@outlook.com>
2021-08-09 15:24:24 +0800

    update .gitignore

--- cash_scheme/rebuild&test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-make clean
-make
-./tests/test_cash_ore < ~/Downloads/pbc-0.5.14/param/d159.param
-# ./tests/time_cash_ore < ~/Downloads/pbc-0.5.14/param/d159.param
--- rebuild&test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-make clean
-make
-./tests/test_ore < ~/Downloads/pbc-0.5.14/param/d159.param
-# ./tests/time_ore < ~/Downloads/pbc-0.5.14/param/d159.param
